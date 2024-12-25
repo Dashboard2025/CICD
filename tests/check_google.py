b6bea22dc95e15b80af1eb9427289ae96962ac00
Dashboard2025 <dashboards.analyticsservices@gmail.com>
2024-12-25 10:10:25 +0400

    Fix: Use latest Chrome and dynamic ChromeDriver version

--- tests/check_google.py
+++ tests/check_google.py
@@ -1,8 +1,5 @@
-# check_google.py
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
